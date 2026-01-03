353688e67457c988624d6b8cd9621ff713a75320
KON HO <56455075+BNLASTKINGHACK@users.noreply.github.com>
2026-01-03 22:52:48 +0530

    Update encryption header and author information

--- bn.py
+++ bn.py
@@ -1,5 +1,5 @@
-# Encrypted by TANMAY PAUL
-# TELEGRAM-: @TANMAYPAUL21
+# Encrypted by BN
+# TELEGRAM-: BN
 
 exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
 "😁 🤣  😁 😁  😅 😉  😃 😃 😉  😃 😃 🤣  😃 😃 😅  😅 😉  😛 😊  😃 😀 🤣  😃 😃 😀  😅 😉  😃 😀 😃\
